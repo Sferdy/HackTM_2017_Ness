a0ad34bb523e3c339e4e1d40867f6ada28253ad0
Andrei <andrei.sferdian@gmail.com>
2017-05-27 18:25:15 +0300

    Update server.py to copy data from a file on the client to a file on the server.

--- server.py
+++ server.py
@@ -6,33 +6,38 @@ from os.path import dirname, abspath, join
 
 SERVER_ADDR = 'localhost'
 PORT = 9999
-inFileName = ''
-
-def stdin2file(file):
-    sys.stdin = file
+inFileName = join(dirname(abspath(__file__)), r'CANoe/HackTM2017_CANoe_Log/Logging.asc')
+outFileName = join(dirname(abspath(__file__)), r'CANoe/HackTM2017_CANoe_Log/outFile.txt')
 
 class ReqHdl(SocketServer.StreamRequestHandler):
 
     def handle(self):
         data = self.request.recv(1024)
-        while data:
-            print "Server received: ", data
-            # echo data back
-            data = data.upper()
-            self.request.sendall(data)
-            print "Server sent: ", data
-            data = self.request.recv(1024)
+        if data:
+            with open(outFileName, 'w+') as outFile:
+                while data:
+                    outFile.write(data)
+                    self.request.send('OK')
+                    data = self.request.recv(1024)
+        # while data:
+        #     print "Server received: ", data
+        #     # echo data back
+        #     data = data.upper()
+        #     self.request.sendall(data)
+        #     print "Server sent: ", data
+        #     data = self.request.recv(1024)
 
 def client_run(server_address):
     target = socket.socket()
     while target.connect_ex(server_address): pass
-    data = sys.stdin.readline()
-    while(data):
-        target.sendall(data)
-        print "Client sent: ", data
-        data = target.recv(1024)
-        print "Client received: ", data
-        data = sys.stdin.readline()
+    with open(inFileName, 'r') as inFile:
+        for line in inFile:
+            target.sendall(line)
+            print "Client sent: ", line
+            response = target.recv(1024)
+            if response == 'OK': pass
+            else: break
+
 
 def server_run(server_address):
     server = SocketServer.ThreadingTCPServer(
