4b356b27be60d68a61468a9cb50032cc989d347d
Miroslav <miroslav@shippiter.com>
2017-05-28 10:42:55 +0200

    -Async fetch data from cloud service THINGSPEAK -Base Parser -Base UI

--- Android_app/HIB/HIB/Resources/Resource.Designer.cs
+++ Android_app/HIB/HIB/Resources/Resource.Designer.cs
@@ -57,6 +57,49 @@ namespace HIB
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int button1 = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int button2 = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int textView1 = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int textView2 = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int textView3 = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int textView4 = 2131034117;
+			
+			// aapt resource value: 0x7f050006
+			public const int textView5 = 2131034118;
+			
+			// aapt resource value: 0x7f050007
+			public const int textView6 = 2131034119;
+			
+			// aapt resource value: 0x7f050008
+			public const int textView7 = 2131034120;
+			
+			// aapt resource value: 0x7f050009
+			public const int textView8 = 2131034121;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
